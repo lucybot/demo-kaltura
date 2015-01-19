9f9cb7de5685eb24f412266f82e83566294264ba
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-01-19 20:28:06 +0000

    Factor out oven into grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,24 @@
 module.exports = function (grunt) {
   // load all npm grunt tasks
   require('load-grunt-tasks')(grunt);
-
+  console.log('load oven');
+  grunt.loadNpmTasks('lucy-oven');
+  console.log('loaded');
   // Project configuration.
   grunt.initConfig({
      jsonlint: {
        sample: {
          src: [ 'cookbook.json' ]
        }
+     },
+     lucy_oven: {
+       mediaList: {
+         dest: 'test/',
+         recipe: 'mediaList'
+       }
      }
   });
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jsonlint']);
+  grunt.registerTask('default', ['lucy_oven']);
 };
