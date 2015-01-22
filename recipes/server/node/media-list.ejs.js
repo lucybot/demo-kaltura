189611d858b94f3d0445c8adc385942906563530
Bobby Brennan <bobby.brennan@gmail.com>
2015-01-22 17:41:36 -0500

    Update media-list.ejs.js

--- recipes/server/node/media-list.ejs.js
+++ recipes/server/node/media-list.ejs.js
@@ -1,8 +1,6 @@
-      var filter = new Kaltura.objects.KalturaMediaEntryFilter();
-      filter.nameLike = <%- mediaList.nameLike %>;
-      var pager = new Kaltura.objects.KalturaFilterPager();
-      KalturaClient.media.listAction(function(results) {
-        console.log('list media:' + JSON.stringify(results));
-        if (results.objectType === 'KalturaAPIException') { console.log('err:' + JSON.stringify(results)); throw results; }
-        res.json(results.objects);
-      }, filter, pager);
+var filter = new Kaltura.objects.KalturaMediaEntryFilter();
+filter.nameLike = <%- mediaList.nameLike.val %>;
+var pager = new Kaltura.objects.KalturaFilterPager();
+KalturaClient.media.listAction(function(results) {
+  res.json(results.objects);
+}, filter, pager);
