6a9fe0474ebe6a705f36be2b7a5680dfdaa9ef90
Bobby Brennan <bobby.brennan@gmail.com>
2015-01-27 18:36:07 -0500

    Update media-list.ejs.js

--- recipes/server/node/media-list.ejs.js
+++ recipes/server/node/media-list.ejs.js
@@ -2,5 +2,5 @@ var filter = new Kaltura.objects.KalturaMediaEntryFilter();
 filter.nameLike = <%- mediaList.nameLike.code %>;
 var pager = new Kaltura.objects.KalturaFilterPager();
 KalturaClient.media.listAction(function(results) {
-  res.json(results.objects);
+  <%- Lucy.return("results.objects") %>
 }, filter, pager);
