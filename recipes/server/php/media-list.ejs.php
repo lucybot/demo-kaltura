540e41d3e5878e524c0e4578091272a829afda44
Bobby Brennan <bobby.brennan@gmail.com>
2015-01-27 13:09:22 -0500

    Update media-list.ejs.php

--- recipes/server/php/media-list.ejs.php
+++ recipes/server/php/media-list.ejs.php
@@ -1,3 +1,4 @@
 $filter = new KalturaMediaEntryFilter();
 $filter->nameLike = <%- mediaList.nameLike.code %>;
-echo json_encode($KalturaClient->media->add(<%- mediaList.entryId.code %>));
+$result = $KalturaClient->media->listAction($filter, null);
+echo json_encode($result);
