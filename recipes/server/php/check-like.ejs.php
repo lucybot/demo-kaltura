01820cd40a7fdac91b24605fc456be6ee8d22f40
Bobby Brennan <bobby.brennan@gmail.com>
2015-01-27 14:25:21 -0500

    Update check-like.ejs.php

--- recipes/server/php/check-like.ejs.php
+++ recipes/server/php/check-like.ejs.php
@@ -1,3 +1,3 @@
 $likePlugin = KalturaLikeClientPlugin::get($KalturaClient);
 $result = $likePlugin->like->checklikeexists(<%- likeButton.videoID %>);
-echo json_encode($result);
+echo json_encode(array('liked' => $result));
