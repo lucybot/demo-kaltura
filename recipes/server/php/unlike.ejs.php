13498683f6a1561b6c3e071d3da7a77c94a470a7
Bobby Brennan <bobby.brennan@gmail.com>
2015-01-28 17:21:32 -0500

    Update unlike.ejs.php

--- recipes/server/php/unlike.ejs.php
+++ recipes/server/php/unlike.ejs.php
@@ -1,3 +1,3 @@
 $likePlugin = KalturaLikeClientPlugin::get($KalturaClient);
-$result = $likePlugin->like->unlike(<%- likeButton.videoID %>);
+$result = $likePlugin->like->unlike(<%- Lucy.answer(likeButton.videoID) %>);
 <%- Lucy.returnCode("array('liked' => !$result)") %>;
