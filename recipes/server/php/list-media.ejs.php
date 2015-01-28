28ecf907cb46ae95f446e0bf2e57276b9ea50766
Bobby Brennan <bobby.brennan@gmail.com>
2015-01-28 17:31:05 -0500

    Update list-media.ejs.php

--- recipes/server/php/list-media.ejs.php
+++ recipes/server/php/list-media.ejs.php
@@ -1,4 +1,4 @@
 $filter = new KalturaMediaEntryFilter();
-$filter->nameLike = <%- Lucy.answer(mediaList.nameLike) %>;
+$filter->nameLike = <%- Lucy.answer('mediaList', 'nameLike') %>;
 $result = $KalturaClient->media->listAction($filter, null);
 <%- Lucy.returnCode("$result") %>;
