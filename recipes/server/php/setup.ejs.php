45fc62e3fa2077494cb7775067ab90827baab92d
Bobby Brennan <bobby.brennan@gmail.com>
2015-01-28 19:53:37 -0500

    Update setup.ejs.php

--- recipes/server/php/setup.ejs.php
+++ recipes/server/php/setup.ejs.php
@@ -1,4 +1,3 @@
-<?php
 require_once('Kaltura/lib/KalturaClient.php');
 $config = new KalturaConfiguration(<%- Lucy.answer('setup', 'partner_id') %>);
 $config->serviceUrl = 'https://www.kaltura.com/';
