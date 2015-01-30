3bcfc3e9e7598ff3d36453c0f66e5b1f15cd84a6
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-01-30 03:59:02 +0000

    PHP working

--- recipes/server/php/list-media.ejs.php
+++ recipes/server/php/list-media.ejs.php
@@ -1,4 +1,4 @@
 $filter = new KalturaMediaEntryFilter();
 $filter->nameLike = <%- Lucy.answer('mediaList', 'nameLike') %>;
-$result = $KalturaClient->media->listAction($filter, null);
+$result = $KalturaClient->media->listAction($filter, null)->objects;
 <%- Lucy.returnCode("$result") %>;
--- recipes/server/php/setup.ejs.php
+++ recipes/server/php/setup.ejs.php
@@ -2,3 +2,10 @@ require_once('lib/KalturaClient.php');
 $config = new KalturaConfiguration(<%- Lucy.answer('setup', 'partner_id') %>);
 $config->serviceUrl = 'https://www.kaltura.com/';
 $KalturaClient = new KalturaClient($config);
+$KalturaSession = $KalturaClient->session->start(
+    <%- Lucy.answer('setup', 'admin_secret') %>,
+    "myUser",
+    KalturaSessionType::ADMIN,
+    <%- Lucy.answer('setup', 'partner_id') %>,
+    null, null);
+$KalturaClient->setKS($KalturaSession);
