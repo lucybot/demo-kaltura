e9817530ff6f2c7751591518a8e6a37c38af834f
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-01-30 02:33:48 +0000

    Add php client lib

--- recipes/server/php/setup.ejs.php
+++ recipes/server/php/setup.ejs.php
@@ -1,4 +1,4 @@
-require_once('Kaltura/lib/KalturaClient.php');
+require_once('lib/Kaltura/Client/Client.php');
 $config = new KalturaConfiguration(<%- Lucy.answer('setup', 'partner_id') %>);
 $config->serviceUrl = 'https://www.kaltura.com/';
 $KalturaClient = new KalturaClient($config);
