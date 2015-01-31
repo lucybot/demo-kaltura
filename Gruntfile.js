97b2c85f14bb458cc2ac5146eadc82ec66be3863
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-01-31 21:54:27 +0000

    Fix gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,28 +13,8 @@ module.exports = function (grunt) {
          src: [ 'cookbook.json' ]
        }
      },
-     lucy_oven: {
-       mediaList: {
-         dest: 'test/',
-         recipe: 'mediaList',
-         answers: {
-           setup: {
-             "admin_secret": "8d6cb692ab0f41bfa6bde373204c4b40",
-             "partner_id": 1760921,
-             "user_id": "anonymous" 
-           },
-           mediaList: {
-             nameLike: '"Kaltura"',
-             likeButton: false
-           },
-           videoPlayer: {
-             skin: '27723522'
-           }
-         }
-       }
-     }
   });
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['lucy_oven']);
+  grunt.registerTask('default', ['jsonlint']);
 };
