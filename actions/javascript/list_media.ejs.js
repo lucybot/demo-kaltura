4d2f994a6699d8826aa879cbc99ed50c59ad1bcf
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-03-11 22:02:11 +0000

    tweaks

--- actions/javascript/list_media.ejs.js
+++ actions/javascript/list_media.ejs.js
@@ -3,6 +3,7 @@ function listMedia() {
     return setTimeout(listMedia, 100);
   }
   var filter = new KalturaMediaEntryFilter();
+  filter.nameLike = <%- Lucy.variable('answers.nameLike') %>;
   var pager = null;
   KalturaClient.media.listAction(function(success, results) {
 <%- Lucy.code.returnCode("results.objects", 4) %>
