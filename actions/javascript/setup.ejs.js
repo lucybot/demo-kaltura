39e4a6d034119f4f5c457f14aef49eba17bb32a4
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-03-12 06:09:36 +0000

    Some tweaks

--- actions/javascript/setup.ejs.js
+++ actions/javascript/setup.ejs.js
@@ -1,10 +1,10 @@
-<script type="text/javascript" src="//bbrennan.info/kaltura/ox.ajast.js"></script>
-<script type="text/javascript" src="//bbrennan.info/kaltura/webtoolkit.md5.js"></script>
-<script type="text/javascript" src="//bbrennan.info/kaltura/KalturaClientBase.js"></script>
-<script type="text/javascript" src="//bbrennan.info/kaltura/KalturaTypes.js"></script>
-<script type="text/javascript" src="//bbrennan.info/kaltura/KalturaVO.js"></script>
-<script type="text/javascript" src="//bbrennan.info/kaltura/KalturaServices.js"></script>
-<script type="text/javascript" src="//bbrennan.info/kaltura/KalturaClient.js"></script>
+<script type="text/javascript" src="http://bbrennan.info/kaltura/ox.ajast.js"></script>
+<script type="text/javascript" src="http://bbrennan.info/kaltura/webtoolkit.md5.js"></script>
+<script type="text/javascript" src="http://bbrennan.info/kaltura/KalturaClientBase.js"></script>
+<script type="text/javascript" src="http://bbrennan.info/kaltura/KalturaTypes.js"></script>
+<script type="text/javascript" src="http://bbrennan.info/kaltura/KalturaVO.js"></script>
+<script type="text/javascript" src="http://bbrennan.info/kaltura/KalturaServices.js"></script>
+<script type="text/javascript" src="http://bbrennan.info/kaltura/KalturaClient.js"></script>
 <script type="text/javascript">
   var KalturaConfig = new KalturaConfiguration(<%- Lucy.code.variable('answers.partnerID') %>);
   KalturaConfig.serviceUrl = "http://www.kaltura.com/";
