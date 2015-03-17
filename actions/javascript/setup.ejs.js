5dd74c6419fc2ebdeee9802b33955fa143578254
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-03-17 23:50:00 +0000

    Move client lib to lucybot.com

--- actions/javascript/setup.ejs.js
+++ actions/javascript/setup.ejs.js
@@ -1,10 +1,4 @@
-<script type="text/javascript" src="http://bbrennan.info/kaltura/ox.ajast.js"></script>
-<script type="text/javascript" src="http://bbrennan.info/kaltura/webtoolkit.md5.js"></script>
-<script type="text/javascript" src="http://bbrennan.info/kaltura/KalturaClientBase.js"></script>
-<script type="text/javascript" src="http://bbrennan.info/kaltura/KalturaTypes.js"></script>
-<script type="text/javascript" src="http://bbrennan.info/kaltura/KalturaVO.js"></script>
-<script type="text/javascript" src="http://bbrennan.info/kaltura/KalturaServices.js"></script>
-<script type="text/javascript" src="http://bbrennan.info/kaltura/KalturaClient.js"></script>
+<script type="text/javascript" src="https://lucybot.com/kaltura/kaltura.min.js"></script>
 <script type="text/javascript">
   var KalturaConfig = new KalturaConfiguration(<%- Lucy.code.variable('answers.partnerID') %>);
   KalturaConfig.serviceUrl = "http://www.kaltura.com/";
