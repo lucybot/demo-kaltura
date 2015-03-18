2b3257c1e28b8513b4f5274f8263c988a73b31e3
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-03-18 01:07:23 +0000

    Move to iframe include

--- actions/javascript/setup.ejs.js
+++ actions/javascript/setup.ejs.js
@@ -1,7 +1,7 @@
 <script type="text/javascript" src="https://lucybot.com/kaltura/kaltura.min.js"></script>
 <script type="text/javascript">
   var KalturaConfig = new KalturaConfiguration(<%- Lucy.code.variable('answers.partnerID') %>);
-  KalturaConfig.serviceUrl = "http://www.kaltura.com/";
+  KalturaConfig.serviceUrl = "https://www.kaltura.com/";
   var KalturaClient = new KalturaClient(KalturaConfig);
   KalturaClient.session.start(function(success, session) {
     KalturaClient.setKs(session);
